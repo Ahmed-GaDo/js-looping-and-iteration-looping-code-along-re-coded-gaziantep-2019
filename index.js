03b7edef2441f24ab4e7998f6c5f27538038039a
Ahmed <a.gado.2df@gmail.com>
2019-07-28 06:15:15 +0300

    Looping Code-along Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["Ada", "Brendan", "Ali"] 
+//const B = "Birthday"
+const B = "birthday"
+//function writeCards (gifts, B);
+// writeCards (["Ada", "Brendan", "Ali"], "Birthday");
+function writeCards (gifts, B){
+    for (let i=0 ; i < gifts.length ; i ++ ) {
+     console.log(`Thank you, ${gifts[i]}, for the wonderful ${B} gift.`);
+    }
+    return gifts 
+}
+console.log(writeCards(gifts, B));
+
+//const number ;
+function countdown (number){
+    let i = number 
+    ///while (i >= 0) {
+     //   console.log(i);
+    //}
+    //i--; }
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    
+
+}
+console.log(countdown(number));
