373a67cb6e5d88b9c7a80a113c626252f5ee6dc0
Ahmed Gado <a.gado.2df@gmail.com>
2019-07-28 02:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const gifts = ["Ada", "Brendan", "Ali"] 
+//const B = "Birthday"
+const B = "birthday"
+//function writeCards (gifts, B);
+// writeCards (["Ada", "Brendan", "Ali"], "Birthday");
+function writeCards (gifts, B){
+    for (let i=0 ; i < gifts.length ; i ++ ) {
+     console.log(`Thank you, ${gifts[i]}, for the wonderful ${B} gift.`);
+    }
+    return gifts 
+}
+console.log(writeCards(gifts, B));
